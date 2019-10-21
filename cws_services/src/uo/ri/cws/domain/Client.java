77020e12318bfb5264b593041a97d2c26ee0845e
Samuel Moreno Vincent <UO266321@uniovi.es>
2019-10-21 11:20:54 +0200

    Añadidos metodos iniciales a cliente

--- cws_services/src/uo/ri/cws/domain/Client.java
+++ cws_services/src/uo/ri/cws/domain/Client.java
@@ -7,6 +7,58 @@ public class Client {
 	private String email;
 	private String phone;
 	private Address address;
+	
+	Client(){}
+	public Client(String dni) {
+		super();
+		this.dni = dni;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Client [dni=" + dni + ", name=" + name + ", surname=" + surname + ", email=" + email + ", phone="
+				+ phone + ", address=" + address + "]";
+	}
 
 }
 
